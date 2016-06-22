716ad08b369bd582c6d545ec8ea91196f6b64f0e
Harold Pulcher <pulcher@pulcher.biz>
2016-06-21 21:51:53 -0500

    got the base in place.  now to add

--- protractor/spec/example_spec.js
+++ protractor/spec/example_spec.js
@@ -1,6 +1,65 @@
 'use strict';
 
-describe('angularjs homepage', function() {
+describe('Protractor Demo App', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
+
+  beforeEach(function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+  });
+
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+
+  it('should add one and two', function() {
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('3');
+  });
+
+  it('should add four and six', function() {
+    firstNumber.sendKeys(6);
+    secondNumber.sendKeys(4);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('10');
+  });
+
+  it('should have a history', function() {
+    add(1, 2);
+    add(3, 4);
+
+    expect(history.count()).toEqual(2);
+
+    add(5, 6);
+
+    expect(history.count()).toEqual(3);
+  });
+
+  it('should have a history in reverse order', function() {
+    add(1, 2);
+    add(3, 4);
+
+    expect(history.last().getText()).toContain('1 + 2');
+    expect(history.first().getText()).toContain('3 + 4');
+  });
+});
+
+//describe('angularjs homepage', function() {
   // it('should greet the named user', function() {
   //   browser.get('http://127.0.0.1:9000');
 
@@ -11,29 +70,29 @@ describe('angularjs homepage', function() {
   //   expect(greeting.getText()).toEqual('Hello Julie!');
   // });
 
-  describe('todo list', function() {
-    var todoList;
+  // describe('todo list', function() {
+  //   var todoList;
 
-    beforeEach(function() {
-      browser.get('http://127.0.0.1:9000');
+  //   beforeEach(function() {
+  //     browser.get('http://127.0.0.1:9000');
 
-      todoList = element.all(by.repeater('todo in todos'));
-    });
+  //     todoList = element.all(by.repeater('todo in todos'));
+  //   });
 
-    it('should list todos', function() {
-      expect(todoList.count()).toEqual(0);
-      //expect(todoList.get(1).getText()).toEqual('build an angular app');
-    });
+  //   it('should list todos', function() {
+  //     expect(todoList.count()).toEqual(0);
+  //     //expect(todoList.get(1).getText()).toEqual('build an angular app');
+  //   });
 
-    it('should add a todo', function() {
-      var addTodo = element(by.model('todo'));
-      var addButton = element(by.css('[value="add"]'));
+  //   it('should add a todo', function() {
+  //     var addTodo = element(by.model('todo'));
+  //     var addButton = element(by.css('[value="add"]'));
 
-      addTodo.sendKeys('write a protractor test');
-      addButton.click();
+  //     addTodo.sendKeys('write a protractor test');
+  //     addButton.click();
 
-      expect(todoList.count()).toEqual(1);
-      expect(todoList.get(2).getText()).toEqual('write a protractor test');
-    });
-  });
-});
+  //     expect(todoList.count()).toEqual(1);
+  //     expect(todoList.get(2).getText()).toEqual('write a protractor test');
+  //   });
+  // });
+//});
